2ee824820901e06b149c974a0b4562213cb62316
MischWynn <lauravlaiina@gmail.com>
2026-08-25 21:49:33 +0700

    chore: remove unrelated file to project Sakuku

--- src/app/core/layout/customer-footer/customer-footer.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-customer-footer',
-  imports: [],
-  templateUrl: './customer-footer.html',
-  styleUrl: './customer-footer.css',
-})
-export class CustomerFooter {}
--- src/app/models/post.model/post.model.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-post.model',
-  imports: [],
-  templateUrl: './post.model.html',
-  styleUrl: './post.model.css',
-})
-export class PostModel {}
