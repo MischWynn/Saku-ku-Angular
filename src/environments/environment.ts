6ec9a2624eebfe1c1df67850c1f7a3f6cfe2cadf
MischWynn <lauravlaiina@gmail.com>
2026-09-20 21:22:28 +0700

    update environment backend gcp

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://ourselves-prediction-village-quarter.trycloudflare.com',
+  apiUrl: 'https://ourselves-prediction-village-quarter.trycloudflare.com/api/v1',
 };
